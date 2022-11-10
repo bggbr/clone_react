e2a84988660fca2007fec0d78a87bdd72ad6fb5f
bggbr <jaejeongbang@hyundai-autoever.com>
2022-11-10 20:27:50 +0900

    navbar, modal 파일 생

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Navbar from "./components/common/Navbar";
 
 function App() {
-    return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+    return (
+        <>
+            <Navbar />
+        </>
+    );
 }
 
 export default App;
---
+++ src/components/common/Navbar.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Navbar() {
+    return <nav className="bg-black fixed w-full h-1/8 text-neutral-50">test</nav>;
+}
+
+export default Navbar;
