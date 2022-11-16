cbee16194c751c97e56808b08779d842bd5a30f2
bggbr <48645202+bggbr@users.noreply.github.com>
2022-11-17 01:48:53 +0900

    Update Row.js

--- src/components/Row.js
+++ src/components/Row.js
@@ -3,108 +3,80 @@ import axios from '../api/axios';
 import requests from '../api/requests';
 
 export default function Row() {
-	// const [movie, setMovie] = useState([]);
-	const [bannerUrl, setBannerUrl] = useState('');
-	const [arrow, setArrow] = useState(false);
-	const [test, setTest] = useState([]);
+    // const [movie, setMovie] = useState([]);
+    const [bannerUrl, setBannerUrl] = useState('');
+    const [arrow, setArrow] = useState(false);
+    const [test, setTest] = useState([]);
 
-	async function fetchMovieGenre() {
-		const test = await axios.get(requests.fetchTrending);
-		// console.log(test.data);
-		// setMovie(test.data.results);
-		// setTest(`https://image.tmdb.org/t/p/original/${test.data.results[5].backdrop_path}`);
-		setBannerUrl(`https://image.tmdb.org/t/p/original/${test.data.results[5].backdrop_path}`);
-	}
+    async function fetchMovieGenre() {
+        const test = await axios.get(requests.fetchTrending);
+        // console.log(test.data);
+        // setMovie(test.data.results);
+        // setTest(`https://image.tmdb.org/t/p/original/${test.data.results[5].backdrop_path}`);
+        setBannerUrl(`https://image.tmdb.org/t/p/original/${test.data.results[5].backdrop_path}`);
+    }
 
-	async function fetchTest() {
-		const test = await axios.get(requests.fetchTrending);
-		const array = [];
-		for (let url of test.data.results) {
-			// console.log(url.backdrop_path);
-			array.push(`https://image.tmdb.org/t/p/original/${url.backdrop_path}`);
-		}
-		setTest([...array]);
-	}
+    async function fetchTest() {
+        const test = await axios.get(requests.fetchTrending);
+        const array = [];
+        for (let url of test.data.results) {
+            // console.log(url.backdrop_path);
+            array.push(`https://image.tmdb.org/t/p/original/${url.backdrop_path}`);
+        }
+        setTest([...array]);
+    }
 
-	const image = useRef();
-	const ttt = useRef([]);
+    const index = useRef(0);
+    const rowImages = useRef([]);
+    const show = 'flex-1 mr-8 transition duration-500 hover:scale-105';
+    const hide = 'hidden flex-0 mr-8 transition duration-500 hover:scale-105';
 
-	const createArrow = () => {
-		// console.log(arrow);
-		setArrow(true);
-	};
+    const createArrow = () => {
+        setArrow(true);
+    };
 
-	const removeArrow = () => {
-		// console.log(arrow);
-		setArrow(false);
-	};
+    const removeArrow = () => {
+        setArrow(false);
+    };
 
-	const slideRight = () => {
-		console.log('clicked');
-	};
+    const slideRight = () => {
+        if (index.current > 18) return;
+        rowImages.current[index.current].className = hide;
+        index.current++;
+        console.log(index.current);
+    };
 
-	useEffect(() => {
-		fetchMovieGenre();
-		fetchTest();
-		setTimeout(() => {
-			console.log(ttt);
-		}, 1000);
-	}, []);
+    const slideLeft = () => {
+        if (index.current < 1) return;
+        rowImages.current[index.current].className = show;
+        index.current--;
+        console.log(index.current);
+    };
 
-	fetchMovieGenre();
-	return (
-		<>
-			<div className='relative w-full h-[12rem] flex overflow-hidden' onMouseOver={createArrow} onMouseLeave={removeArrow}>
-				{test.map((v, i) => (
-					<div className='inline-block flex-1 w-50 h-full m-1'>
-						<img
-							className='w-10 h-10'
-							src={v}
-							alt='img'
-							ref={(el) => {
-								ttt.current[i] = el;
-							}}
-						/>
-					</div>
-				))}
-				{/* <img
-					className={'w-10 h-10 absolute top-20 cursor-pointer ' + (arrow ? ' ' : ' hidden')}
-					src='/assets/icon/left-arrow.png'
-					alt='left'
-				/> */}
-				{/* <div className='inline-block flex-1 w-50 h-full m-1 bg-green-500'></div>
-			<div className='inline-block flex-1 w-50 h-full m-1 bg-red-500'></div>
-			<div className='inline-block flex-1 w-50 h-full m-1 bg-blue-500'></div>
-			<div className='inline-block flex-1 w-50 h-full m-1 bg-yellow-500'></div> */}
-				{/* <div className='inline-block flex-1 w-50 h-full m-1'>
-					<img className='block h-full hover:h-60' ref={image} src={bannerUrl} alt='test' />
-				</div>
-				<div className='inline-block flex-1 w-50 h-full m-1'>
-					<img className='block h-full hover:h-60' ref={image} src={bannerUrl} alt='test' />
-				</div>
-				<div className='inline-block flex-1  w-50 h-full m-1'>
-					<img className='block h-full hover:h-60' ref={image} src={bannerUrl} alt='test' />
-				</div>
-				<div className='inline-block flex-1  w-50 h-full m-1'>
-					<img className='block h-full hover:h-60' ref={image} src={bannerUrl} alt='test' />
-				</div>
-				<div className='inline-block flex-1  w-50 h-full m-1'>
-					<img className='block h-full hover:h-60' ref={image} src={bannerUrl} alt='test' />
-				</div>
-				<img
-					className={'w-10 h-10 absolute top-20 right-0 cursor-pointer ' + (arrow ? ' ' : ' hidden')}
-					src='/assets/icon/right-arrow.png'
-					alt='right'
-					onClick={slideRight}
-				/> */}
-			</div>
-			{/* <div className='flex flex-nowrap overflow-hidden '>
-				// <div className='w-[500px] h-full bg-green-500 m-10'>test</div>
-				// <div className='w-[500px] h-full bg-yellow-500 m-10'>test</div>
-				// <div className='w-[500px] h-full bg-red-500 m-10'>test</div>
-				// <div className='w-[500px] h-full bg-blue-500 m-10'>test</div>
-				// <div className='w-[500px] h-full bg-black m-10'>test</div>
-			</div> */}
-		</>
-	);
+    useEffect(() => {
+        fetchMovieGenre();
+        fetchTest();
+    }, []);
+
+    fetchMovieGenre();
+
+    return (
+        <div className="relative w-full h-[20rem] flex overflow-hidden p-10 bg-black" onMouseOver={createArrow} onMouseLeave={removeArrow}>
+            <img className={'w-8 h-12 z-50 rotate-180 absolute top-36 cursor-pointer ' + (arrow ? '' : 'hidden')} src="/assets/icon/right-arrow.png" alt="left" onClick={slideLeft} />
+
+            {test.map((v, i) => (
+                <div
+                    className="flex-1 mr-8 transition duration-500 hover:scale-105"
+                    key={i}
+                    ref={(el) => {
+                        rowImages.current[i] = el;
+                    }}
+                >
+                    <img className="h-full " src={v} alt="img" />
+                </div>
+            ))}
+
+            <img className={'w-8 h-12 z-50  absolute top-36 right-12 cursor-pointer ' + (arrow ? '' : 'hidden')} src="/assets/icon/right-arrow.png" alt="right" onClick={slideRight} />
+        </div>
+    );
 }
