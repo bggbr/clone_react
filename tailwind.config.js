f2e9483a72ef011262eafad0a192f455ae930663
bggbr <48645202+bggbr@users.noreply.github.com>
2022-11-17 01:49:26 +0900

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{js,jsx,ts,tsx}'],
-	theme: {
-		extend: {
-			backgroundImage: (theme) => ({
-				banner_image: `https://image.tmdb.org/t/p/original${theme}`,
-			}),
-			height: {
-				192: '48rem',
-			},
-			flex: {
-				1: '1 0 25%',
-			},
-		},
-	},
-	plugins: [],
+    content: ['./src/**/*.{js,jsx,ts,tsx}'],
+    theme: {
+        extend: {
+            backgroundImage: (theme) => ({
+                banner_image: `https://image.tmdb.org/t/p/original${theme}`,
+            }),
+            height: {
+                192: '48rem',
+            },
+            flex: {
+                1: '1 0 auto',
+            },
+        },
+    },
+    plugins: [],
 };
