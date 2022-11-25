45fb55d3e8ccadb6a1d9c011ea32e00210a31755
bggbr <jaejung603@gmail.com>
2022-11-25 22:19:05 +0900

    Swiper 오류 수정

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,9 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import MovieModal from './common/MovieModal';
 import axios from '../api/axios';
+
 import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
 
 import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
 
 export default function Row({ isLargeRow, title, id, fetchUrl }) {
 	const [movies, setMovies] = useState([]);
@@ -26,13 +33,15 @@ export default function Row({ isLargeRow, title, id, fetchUrl }) {
 	}, []);
 
 	return (
-		<div className='bg-black relative flex'>
-			<h1 className='text-white text-2xl font-bold ml-10 pt-4'>{title}</h1>
+		<div className='bg-black relative'>
+			<h1 className='block text-white text-2xl font-bold mx-10 py-4'>{title}</h1>
 			<Swiper
+				className='mx-10'
 				// install Swiper modules
 				modules={[Navigation, Pagination, Scrollbar, A11y]}
 				navigation
-				pagination={{ clickable: true }}
+				loop={true}
+				// pagination={{ clickable: true }}
 				breakpoints={{
 					1378: {
 						slidesPerView: 6,
@@ -52,21 +61,18 @@ export default function Row({ isLargeRow, title, id, fetchUrl }) {
 					},
 				}}
 				onSlideChange={() => console.log('slide change')}
-				// spaceBetween={0}
-				// slidesPerView={3}
-				// scrollbar={{ draggable: true }}
-				// onSwiper={(swiper) => console.log(swiper)}
 			>
 				<div
 					id={id}
-					className='relative w-full h-[20rem] flex overflow-hidden px-10 py-4 bg-black'
-					onMouseOver={() => setArrow(true)}
-					onMouseLeave={() => setArrow(false)}
+					className='relative w-full h-[20rem] flex overflow-hidden bg-black'
+					// onMouseOver={() => setArrow(true)}
+					// onMouseLeave={() => setArrow(false)}
 				>
 					{movies.map((movie) => (
-						<SwiperSlide key={movie.id}>
-							<div className='flex-1 mr-8 transition duration-500 hover:scale-105'>
+						<SwiperSlide>
+							<div className='flex-1 mr-8 transition duration-500 hover:scale-110'>
 								<img
+									key={movie.id}
 									className='h-full cursor-pointer'
 									src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
 									alt='img'
